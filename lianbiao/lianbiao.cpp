7fa497866da0d0c3abeedb7198f6f1c903769419
yunhuaCN <z1521590789@163.com>
2024-04-05 23:44:11 +0800

    --feat: 链表代码学习

---
+++ lianbiao/lianbiao.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        //delete head node
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode();
+    cout << head->val;
+    return 0;
+}
